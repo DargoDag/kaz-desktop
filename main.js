9bd30bdf87656ab4237889df860e6a5aacc7f210
Magomed Magomedov <dargojs@gmail.com>
2023-06-20 22:18:08 +0300

    fix: small fix

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
+$('#banner-slider').slick({
+  prevArrow: $('.prev-btn'),
+  nextArrow: $('.next-btn')
+});
+$('#img-slider').slick({
+  prevArrow: $('.btn-prev'),
+  nextArrow: $('.btn-next'),
+  vertical: true,
+  slidesToShow: 3,
+  arrows: true,
+});
 
-$(document).ready(function () {
-  $('#banner-slider').slick({
-    prevArrow: $('.prev-btn'),
-    nextArrow: $('.next-btn')
-  });
-  $('#img-slider').slick({
-    prevArrow: $('.btn-prev'),
-    nextArrow: $('.btn-next'),
-    vertical: true,
-    slidesToShow: 3,
-    arrows: true,
-  });
+$('.select__item').click(function () {
+  $('.select__item').removeClass('active');
+  $(this).addClass('active');
 
-  $('.select__item').click(function () {
-    $('.select__item').removeClass('active');
-    $(this).addClass('active');
+  let color = $(this).attr('class').split(' ')[1];
 
-    let color = $(this).attr('class').split(' ')[1];
-
-    $('.card__img').removeClass('active');
-    $('.card__img.' + color).addClass('active');
-  });
-
+  $('.card__img').removeClass('active');
+  $('.card__img.' + color).addClass('active');
 });
